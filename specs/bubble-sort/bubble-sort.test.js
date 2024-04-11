4b14b12523b19ea0522bee2a7fe70fa034f0a90c
zestx5 <buyankov.ilya@gmail.com>
2024-04-11 10:15:15 +0300

    bubble first try

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,13 +11,34 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let swap = false;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let tmp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = tmp;
+        swap = true;
+      }
+    }
+    if (!swap) {
+      break;
+    }
+    swap = false;
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test("sorted", function () {
+  const nums = [1, 2, 3, 4];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4]);
 });
