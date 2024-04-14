f35352b46beb54428af577d15ee3e05e1138cd08
zestx5 <buyankov.ilya@gmail.com>
2024-04-14 13:30:54 +0300

    merge

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -8,11 +8,29 @@
 
 const mergeSort = (nums) => {
   // code goes here
-};
+  if (nums.length < 2) {
+    return nums;
+  }
 
+  let middle = Math.floor(nums.length / 2);
+  let left = nums.slice(0, middle);
+  let right = nums.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+};
+const merge = (a, b) => {
+  let out = [];
+  while (a.length && b.length) {
+    if (a[0] <= b[0]) {
+      out.push(a.shift());
+    } else {
+      out.push(b.shift());
+    }
+  }
+  return out.concat(a, b);
+};
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
