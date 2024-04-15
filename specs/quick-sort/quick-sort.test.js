173ac1d6305b9e3fc7134d46e8e7bc8cbef98de6
zestx5 <buyankov.ilya@gmail.com>
2024-04-15 09:24:21 +0300

    quick sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,31 @@
 
 function quickSort(nums) {
   // code goes here
+  // base case
+
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  let pivot = nums[nums.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (pivot < nums[i]) {
+      right.push(nums[i]);
+    } else {
+      left.push(nums[i]);
+    }
+  }
+  left = quickSort(left);
+  right = quickSort(right);
+
+  return left.concat(pivot, right);
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
